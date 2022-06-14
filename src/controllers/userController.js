cebce96f2883a8124414e01c41b75bb125c460c2
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2022-06-14 13:44:03 -0500

    [+] View/Controller: Desplegar errores del formulario de login, por campo

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -13,16 +13,41 @@ exports.registerAccount = async ( request, response ) => {
         // Inserta nuevo usuario
         const user = await Users.create({ email, password });
 
-        console.log( user.id );
+        // console.log( user.id );
         response.redirect( '/login' );
 
     }
     catch ( err ) {
-        console.error( err );
+        let errors = {
+            email: [],
+            password: [],
+        };
+
+        // console.error( err );
+
+        // * Itera los errores producidos por las validaciones y restricciones del Modelo en Sequelize
+        for( const error of err.errors ) {
+            // * Compila todos los errores relacionados al email
+            if( error.path === 'email' )
+                errors.email.push({ 
+                    path: error.path,
+                    validatorKey: error.validatorKey,
+                    message: error.message
+                });
+            // * Compila todos los errores relacionados al password
+            if( error.path === 'password' )
+                errors.password.push({ 
+                    path: error.path,
+                    validatorKey: error.validatorKey,
+                    message: error.message
+                });
+        }
+
+        // console.log( errors );
 
         response.render( 'create-account', {
             name_page: 'Crear cuenta en UpTask',
-            errors: err.errors
+            errors
         });
     }
 
